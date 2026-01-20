5b1b439b0b9c8f865529efa9388b8c22d2fa1eea
Chinmayee <chinmayeesr2@gmail.com>
2026-01-20 17:16:30 +0530

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/global.css";
+import "./Styles/global.css";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -9,6 +9,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
-
 
 
